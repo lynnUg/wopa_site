9b7bb628eb5a9fb1bf7e75927756c9a0984d5e3d
lynnug <lynnasiimwe@gmail.com>
2014-07-18 14:53:31 +0300

    updating model

--- wopa_submitter/models.py
+++ wopa_submitter/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
-DEFAULT_EXAM_ID = 1
+#Create your models here.
 class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
 
 class Assignment(models.Model):
     name = models.CharField(max_length=128)
     about=models.TextField();
-    studentRelate = models.ManyToManyField(User, through='StuAssign',related_name='studentRelate')
+    sent=models.BooleanField(default=False)
     def __unicode__(self):
         return self.name
 
@@ -18,6 +17,6 @@ class StuAssign(models.Model):
     marker=models.CharField(max_length=128)
     feedback=models.TextField()
     marked=models.BooleanField(default=False)
-    documents = models.ForeignKey(Document,default=DEFAULT_EXAM_ID)
+    documents = models.ForeignKey(Document,null=True)
     date_submitted = models.DateField()
 
