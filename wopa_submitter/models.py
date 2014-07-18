d83e851435ad9bc51dfb945f50c425bb9c2eb620
lynnug <lynnasiimwe@gmail.com>
2014-07-18 15:08:41 +0300

    updating models once again

--- wopa_submitter/models.py
+++ wopa_submitter/models.py
@@ -7,7 +7,7 @@ class Document(models.Model):
 class Assignment(models.Model):
     name = models.CharField(max_length=128)
     about=models.TextField();
-    sent=models.BooleanField(default=False)
+    publish=models.BooleanField(default=False)
     def __unicode__(self):
         return self.name
 
